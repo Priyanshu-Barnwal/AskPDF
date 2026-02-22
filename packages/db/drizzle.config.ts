81efd7ed8de40c0e0e120636cf96a85310648151
priyanshubarnw <barnwal.priyanshu4@gmail.com>
2026-02-22 23:58:59 +0530

    db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export default defineConfig({
-    schema: './schema.ts',
-    out: './migrations',
-    dialect: 'postgresql',
+    schema: "./src/schema.ts",
+    out: "./migrations",
+    dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
     },
